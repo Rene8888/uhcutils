b10e6c31c0820dc7f86a128b71ce1ce4364ca460
Rene8888 <rene.hollander@hotmail.de>
2014-03-02 16:03:32 +0100

    fixed a bug

--- src/main/java/com/exsoloscript/uhcutils/UHCutils.java
+++ src/main/java/com/exsoloscript/uhcutils/UHCutils.java
@@ -7,6 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.exsoloscript.player.UHCPlayerManager;
 import com.exsoloscript.uhcutils.cmd.JoinCommands;
 import com.exsoloscript.uhcutils.event.LoginListener;
+import com.exsoloscript.uhcutils.team.Teams;
 
 public class UHCutils extends JavaPlugin {
 
