a61aff0533cebf05ca7de7b55cc9347d0242918a
Rene8888 <rene.hollander@hotmail.de>
2014-03-04 11:20:04 +0100

    worked more on commands

--- src/main/java/com/exsoloscript/uhcutils/cmd/TeamCommands.java
+++ src/main/java/com/exsoloscript/uhcutils/cmd/TeamCommands.java
@@ -25,30 +25,41 @@ public class TeamCommands extends AbstractCommand {
 			UHCPlayer player = UHCutils.getUHCPlayerManager().getPlayer((Player) sender);
 			if (label.equalsIgnoreCase("join")) {
 				if (args.length == 1) {
-					try {
-						int id = Integer.parseInt(args[0]);
-						Team t = UHCutils.getTeams().getTeamByID(id);
-						if (t == null) {
-							sender.sendMessage(UHCutils.prefix() + ChatColor.RED + "No Team found with ID: " + id);
-							return true;
-						} else {
-							boolean added = t.addPlayer(player);
-							if (added) {
-								sender.sendMessage(UHCutils.prefix() + ChatColor.RED + "You successfully joined team: " + id);
-								if (t.getTeamSize() > 1) {
-									sender.sendMessage(UHCutils.prefix() + ChatColor.RED + "Your team members are: ");
-									for (UHCPlayer member : t.getPlayerList()) {
-										sender.sendMessage(UHCutils.prefix() + ChatColor.RED + "Player: " + member.getName());
-									}
-								}
+					sender.sendMessage(UHCutils.prefix() + ChatColor.RED + "Invalid use of command!");
+					return true;
+				} else if (args.length == 2) {
+					// TODO add support for admins to move a user
+					if (args[0].equalsIgnoreCase("team")) {
+						try {
+							int id = Integer.parseInt(args[0]);
+							Team t = UHCutils.getTeams().getTeamByID(id);
+							if (t == null) {
+								sender.sendMessage(UHCutils.prefix() + ChatColor.RED + "No Team found with ID: " + id);
 								return true;
 							} else {
-
+								boolean added = t.addPlayer(player);
+								if (added) {
+									sender.sendMessage(UHCutils.prefix() + ChatColor.WHITE + "You successfully joined team: " + id);
+									if (t.getTeamSize() > 1) {
+										sender.sendMessage(UHCutils.prefix() + ChatColor.WHITE + "Your team members are: ");
+										for (UHCPlayer member : t.getPlayerList()) {
+											sender.sendMessage(UHCutils.prefix() + ChatColor.WHITE + "Player: " + member.getName());
+										}
+									}
+									return true;
+								} else {
+									return false;
+								}
 							}
+						} catch (Exception e) {
+							sender.sendMessage(UHCutils.prefix() + ChatColor.RED + "The first argument hast do be a number!");
+							return true;
 						}
+					} else if (args[0].equalsIgnoreCase("player")) {
+						// TODO add command to join player to a team
 						return true;
-					} catch (Exception e) {
-						sender.sendMessage(UHCutils.prefix() + ChatColor.RED + "The first argument hast do be a number!");
+					} else {
+						sender.sendMessage(UHCutils.prefix() + ChatColor.RED + "unkown parameter: " + args[0] + "!");
 						return true;
 					}
 				} else {
